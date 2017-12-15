b7154cd03b1eecdd1e39ac9690b4da1f6d5dcc40
DivyanshPatel <nealbay@gmail.com>
2017-12-15 14:02:40 +0530

    Render Album function to return list of albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,17 +3,22 @@ import {View , Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
-  state = {album: [] };
+  state = {albums: [] };
   componentWillMount(){
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
     .then(response => this.setState({albums: response.data}));
   }
-  
+
+ renderAlbum(){
+   return this.state.albums.map( album => <Text>{album.title}</Text>)
+ }
+
+
   render() {
     console.log(this.state);
     return (
       <View>
-        <Text>AlbumList!!!</Text>
+        {this.renderAlbum()}
       </View>
     );
   }
