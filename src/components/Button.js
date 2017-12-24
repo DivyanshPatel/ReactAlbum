da1d15244828af285b17db8f87b992793f52d377
DivyanshPatel <nealbay@gmail.com>
2017-12-25 03:04:06 +0530

    Added Button and Text Style

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,32 @@ import {Text , TouchableOpacity} from 'react-native';
 
 const Button = () => {
   return (
-    <TouchableOpacity>
-      <Text>Click me!!!! </Text>
+    <TouchableOpacity style={styles.buttonStyle}>
+      <Text style = {styles.textStyle}>Click me!!!! </Text>
     </TouchableOpacity>
 
   );
+};
+
+const styles = {
+  textStyle:{
+    alignSelf: 'center',
+    fontSize: 16,
+    color: '#007aff',
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderColor: '#007aff',
+    borderWidth: 1,
+    marginLeft: 5,
+    marginRight: 5
+  }
 };
 
 export default Button;
